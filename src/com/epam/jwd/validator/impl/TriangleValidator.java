d0d0cea99aacaf2cd9e8fe079707e179340b0d9d
vladmaiski <cs_strim@mail.ru>
2021-04-21 18:08:43 +0300

    Add isValid() impl

--- src/com/epam/jwd/validator/impl/TriangleValidator.java
+++ src/com/epam/jwd/validator/impl/TriangleValidator.java
@@ -1,6 +1,8 @@
 package com.epam.jwd.validator.impl;
 
 import com.epam.jwd.model.polygonal.Triangle;
+import com.epam.jwd.model.simple.Point;
+import com.epam.jwd.service.PointService;
 import com.epam.jwd.validator.api.ShapeValidator;
 
 public class TriangleValidator implements ShapeValidator<Triangle> {
@@ -19,7 +21,22 @@ public class TriangleValidator implements ShapeValidator<Triangle> {
 
     @Override
     public boolean isValid(Triangle shape) {
-        return false;
+        double[] arrOfSidesLength = calcFigureSidesLength(shape);
+
+        return arrOfSidesLength[0] < (arrOfSidesLength[1] + arrOfSidesLength[2])
+                && arrOfSidesLength[1] < (arrOfSidesLength[0] + arrOfSidesLength[2])
+                && arrOfSidesLength[2] < (arrOfSidesLength[0] + arrOfSidesLength[1]);
+    }
+
+    public double[] calcFigureSidesLength(Triangle shape) {
+        Point[] points = shape.getPoints();
+        double[] arrOfSidesLength = new double[3];
+        PointService pointService = PointService.getInstance();
+        arrOfSidesLength[0] = pointService.calcLenBetweenPoints(points[0], points[1]);
+        arrOfSidesLength[1] = pointService.calcLenBetweenPoints(points[0], points[2]);
+        arrOfSidesLength[2] = pointService.calcLenBetweenPoints(points[1], points[2]);
+
+        return arrOfSidesLength;
     }
 
 }
